1b26951adc8510e2b14f53d40a2cec9c13aee3bf
kishansinghchauhan <kishancoer7071@gmail.com>
2020-04-20 16:44:09 +0530

    add signup and database in firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './containers/shop page/shoppage';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './containers/sign-in-and-sign-up/sign-in-and-sign-up';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,12 +20,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
+
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
